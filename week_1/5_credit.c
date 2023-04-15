bcf3247a1927250cadaa4f2f694d6527cfdfe25f
4ayokk <113011334+4ayokk@users.noreply.github.com>
2023-04-15 20:40:20 +0300

    Update 5_credit.c

--- week_1/5_credit.c
+++ week_1/5_credit.c
@@ -1,22 +1,31 @@
+// Підключаєм необхідні бібліотеки
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void){
+int main(void) {
+    // Змінна яка зберігатиме номер картки
     long number;
+    
+    // цикл виокнуватиметься до поки користувач не введе число більше 0
     do{
         number = get_long("Number: ");
     }while(number < 0);
-
+    
+    // змінна яка зберігатиме значення контрольної суми за алгоритмом Луга
     int sum = 0;
+    
+    // сума всіх других розрядів помножених на два
     for(long n = number / 10; n > 0; n /= 100){
         long temp_number = n % 10 * 2;
         sum += temp_number / 10;
         sum += temp_number % 10;
     }
+    // + решта розрядів
     for(long n = number; n > 0; n /= 100){
         sum += n % 10;
     }
     
+    // превірка на коректність та відповідність форматам
     if(sum % 10 == 0 && (number / 10000000000000 == 34 || number / 10000000000000 == 37)){
         printf("AMEX\n");
     } else if(sum % 10 == 0 && (number / 1000000000000 == 4 || number / 1000000000000000 == 4)){
@@ -26,5 +35,6 @@ int main(void){
     }else {
         printf("INVALID\n");
     }
+    
     return 0;
 }
